516b139fde31e6bfe79bd5c111da03aad950628f
Zain Khan <99469190+zain6khan@users.noreply.github.com>
2024-03-15 23:37:51 -0400

    Update script.py

--- script.py
+++ script.py
@@ -26,7 +26,7 @@ def scrape_data_point():
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        target_element = soup.find("a", class_="standard-link")
+        target_element = soup.find("h3", class_="standard-link")
         data_point = "" if target_element is None else target_element.text
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
