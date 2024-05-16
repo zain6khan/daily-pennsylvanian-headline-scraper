8d016147a9c3ec3e11295812f2a665ecfe0b2cac
Zain Khan <zain6khan@gmail.com>
2024-05-16 18:24:25 -0400

    fixed caching issue

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ def scrape_data_point():
     Returns:
         str: The headline text if found, otherwise an empty string.
     """
-    req = requests.get("https://www.thedp.com/section/sports")
+    req = requests.get("https://www.thedp.com/section/sports", headers={"Cache-Control": "no-cache"})
     loguru.logger.info(f"Request URL: {req.url}")
     loguru.logger.info(f"Request status code: {req.status_code}")
 
